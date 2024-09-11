d626d3ad349e0d501c19df6660b599506a44271c
ayofatoye <afatoye@unomaha.edu>
2024-09-11 03:25:45 +0000

    Finished Lab 1 (Age and MadLib)

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,16 +6,18 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("heyyyy")
   #Ask for the user's name
-
+  name = input("Enter your name: ")
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
-
+  age = input("Enter your age: ")
+  age = int(age) #convert from words to number
   #Tell the user what year they were born in.
+  born = 2024 - age
   #Assume that they have not had their birthday yet this year.
-
+  print("You were born in", born)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
--- MadLib.py
+++ MadLib.py
@@ -6,10 +6,12 @@
 def main():
   print("Madlib")
   #Ask user for words
+  noun1 = input("Enter a noun: ")
+  noun2 = input("Enter a noun: ")
 
   #Print the story with the user supplied words.
-
-
+  print("I like to ride a " + noun1 + "with my friend " + noun2 + "is fun.")
+  print("I like to ride a", noun1, "with my friend", noun2, "is fun.")
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
